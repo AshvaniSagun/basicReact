032eaf2577e8e48014d42c0879c918a91af85eef
z004erjb <ashvani.sagun@siemens.com>
2022-01-19 01:32:39 +0530

    useState done

--- src/components/common/main.js
+++ src/components/common/main.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Home } from '../home/home'; //Example of named export.
 import  GetRequest from '../getApi/GetRequest';
-import Topics from '../pricing/pricing';  //Its an ES6 feature called default export.
+import Topics from '../../components/topics/Topics';  //Its an ES6 feature called default export.
 
 //The / route will only match when the pathname is exactly the string "/"
 const Main = () => (
---
+++ src/components/topics/Topics.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from 'react'
+
+export default function Topics() {
+    const [text, setText] = useState('Hello There !!');
+
+    const changeText = function() {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+
+    const emitText = function(event) {
+        setText(event.target.value)
+    }
+
+    return (
+        <div>
+            <h1>Change the state - {text}</h1>
+            <div className="container my-3">
+            <div className="mb-3">
+                <textarea  className="form-control" id="myBox" onChange={emitText} value = {text} rows="8"></textarea>
+            </div>
+            <button className="btn btn-primary" onClick={changeText}>click Me!</button>
+            </div>
+        </div>
+    )
+}
