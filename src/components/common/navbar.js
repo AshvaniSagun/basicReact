859dfcf085598df4920b8ff6da49e74250c9b9fd
AshvaniSagun <ashvanisagun@gmail.com>
2022-01-27 15:54:42 +0530

    Added about component.

--- src/components/common/navbar.js
+++ src/components/common/navbar.js
@@ -13,8 +13,8 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
             <li className="nav-item nav-link active" href="#"> <Link to="/">{props.title}</Link><span className="sr-only"></span></li>
-            <li className="nav-item nav-link" href="#"><Link to="editor">{props.otherNavs.link2}</Link></li>
             <li className="nav-item nav-link" href="#">  <Link to="getApi">{props.otherNavs.link1}</Link></li>
+            <li className="nav-item nav-link" href="#"><Link to="editor">{props.otherNavs.link2}</Link></li>
             <li className="nav-item nav-link" href="#">  <Link to="about">{props.otherNavs.link3}</Link></li>
             {/* <li className="nav-item nav-link disabled" href="#">{props.otherNavs.link3}</li> */}
           </div>
