920d0d8b4cd3d4f0b1d54eb0217634b44b7c29d6
AshvaniSagun <ashvanisagun@gmail.com>
2022-02-15 23:52:52 +0530

    added chip style

--- src/components/chip/chip.jsx
+++ src/components/chip/chip.jsx
@@ -16,8 +16,10 @@ export default function ChipDemo() {
             <div className="input-group">
                 <Chips placeholder="Type to search" value={values} max={4} onChange={(e) => setValues(e.value)} />
                 <i onClick={clearAllChips} tooltip="Clear all chips" className="pi pi-times cross-button"></i>
-                <span style={{ cursor: "pointer" }} className="p-inputgroup-addon"><img alt="SearchIcon" src={search} /></span>
-                <span style={{ cursor: "pointer" }} className="p-inputgroup-addon"> <img alt="DownloadIcon" src={download} /></span>
+                <span style={{ cursor: "pointer", position: "relative", right: "2%" }} className="p-inputgroup-addon">
+                    <img style={{ left: "4px", position: "relative" }} alt="SearchIcon" src={search} />&nbsp;&nbsp;
+                    <img style={{ right: "4px", position: "relative" }} alt="DownloadIcon" src={download} />
+                </span>
             </div>
         </div>
     )
